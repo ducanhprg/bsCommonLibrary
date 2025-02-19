a91e8f3ac565b53b36a100726869fdc6191d9cd8
Travis Bui <travisbui@ezsolve.ai>
2025-02-19 10:17:23 +0700

    update keys for shipsaving integration

--- shared/constants/endpointsVendors.ts
+++ shared/constants/endpointsVendors.ts
@@ -1,5 +1,30 @@
 export const ENDPOINTS : any[] = [
     {
+      name: 'Shipsaving',
+      environment: 'production',
+      apiKey: '',
+      vendorDomain : "https://s.shipsaving.com",
+      tokenEndpoint : "https://s.shipsaving.com",
+      createOrderEndpoint : "https://s.shipsaving.com/api/rates/buy-label",
+      cancelOrderEndpoint : "https://s.shipsaving.com/api/shipments/void",
+      getLabelEndpoint : "https://s.shipsaving.com",
+      rateOrderEndpoint : 'https://s.shipsaving.comapi/rates/list',
+    },
+    {
+      name: 'Shipsaving',
+      environment: 'development',
+      apiKey: '',
+      vendorDomain : "https://www.shipsaving.us",
+      tokenEndpoint : "https://www.shipsaving.us",
+      createOrderEndpoint : "https://www.shipsaving.us/api/rates/buy-label",
+      cancelOrderEndpoint : "https://www.shipsaving.us/api/shipments/void",
+      getLabelEndpoint : "https://www.shipsaving.us",
+      rateOrderEndpoint : 'https://shipsaving.us/api/rates/list',
+    },
+    {
+      // api document -> https://www.showdoc.com.cn/IOTEasy/4410471878010502
+      // client_id 账号：Dev
+      // secret 密码：Ml4eYCu6PI6z
       name: 'GDE',
       environment: 'production',
       apiKey: '',
